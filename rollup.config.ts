83cc85ec0b161a79eb3e7fd6e9279b56a260dae5
osaario <osaario1@gmail.com>
2018-11-03 10:52:30 +0200

    clean stuff from declarative components

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'declarative-components'
+const libraryName = 'react-good-form'
 
 export default {
   input: `src/${libraryName}.ts`,
---
+++ src/react-good-form.ts
@@ -0,0 +1,8 @@
+export * from './Form'
+export * from './formrules'
+
+export default {}
+
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
